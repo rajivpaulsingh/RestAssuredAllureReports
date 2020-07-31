8bf5c7289812d8c2ab375d3830f4f4325e8b278c
Singh <singh2rajiv@dgb-c02vh08bh.lan>
2020-07-31 16:31:57 -0700

    Query parameters using HashMap

--- src/test/java/com/studentapp/tests/MyFirstTest.java
+++ src/test/java/com/studentapp/tests/MyFirstTest.java
@@ -3,12 +3,14 @@ package com.studentapp.tests;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import io.restassured.RestAssured;
+//import io.restassured.RestAssured;
 import io.restassured.response.Response;
 //import io.restassured.response.ValidatableResponse;
 //import io.restassured.specification.RequestSpecification;
 
 import static io.restassured.RestAssured.*;
+
+import java.util.*;
 
 public class MyFirstTest {
 	
@@ -48,9 +50,14 @@ public class MyFirstTest {
 	@Test
 	void getSingleCSStudent() {
 		
+		Map<String, Object> hmap = new HashMap<String, Object>();
+		hmap.put("programme", "Computer Science");
+		hmap.put("limit", 2);
+		
 		Response res = given()
-		.queryParam("programme", "Computer Science")
-		.queryParam("limit", 2)
+//		.queryParam("programme", "Computer Science")
+//		.queryParam("limit", 2)
+		.queryParams(hmap)
 		.when()
 		.get("http://localhost:8085/student/list");
 		
